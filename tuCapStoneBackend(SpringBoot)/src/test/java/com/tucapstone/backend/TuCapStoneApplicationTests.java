b9b40a7ada929298fa27a53930f62ee3593de587
ckmckmckm243@gmail.com <171314185+raccoonmin123@users.noreply.github.com>
2026-05-08 19:38:39 +0900

    create SpringBoot

---
+++ tuCapStoneBackend(SpringBoot)/src/test/java/com/tucapstone/backend/TuCapStoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tucapstone.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuCapStoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
